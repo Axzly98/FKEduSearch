f4d5284b3b6916bb07bca6b7c91cfd35829dc156
amin <55675998+amin.ramli@users.noreply.github.com>
2023-06-02 01:27:04 +0800

    header

---
+++ Expert/header.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>FK-EduSearch</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link href="assets/img/logoUMPIcon.jpeg" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+  <!-- ========================================================
+  * Template Name: Append
+  * Updated: May 30 2023 with Bootstrap v5.3.0
+  * Template URL: https://bootstrapmade.com/append-bootstrap-website-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body class="blog-page">
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header position-relative d-flex align-items-center scroll-up-sticky">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+
+      <a href=" " class="logo d-flex align-items-center me-auto me-xl-0">
+
+        <img src="assets/img/logoUMP.png" alt="Image description" width="auto" height="auto" class="image-class" id="image-id">
+        <span></span>
+      </a>
+	  
+	  <nav id="navmenu" class="navmenu">
+        <ul>
+          <li style="color : #18A0FB ; font-weight: bold">Universiti Malaysia Pahang</li>
+        </ul>
+		
+		
+
+       
+      </nav>
+
+      <a style="color : #18A0FB"><?php echo "<b> Date Today: ".date("j F ,  Y")."</b>"; ?></a>
+
+    </div>
+  </header><!-- End Header -->
+  
+<hr>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header position-relative d-flex align-items-center scroll-up-sticky">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+
+      <a href=" " class="logo d-flex align-items-center me-auto me-xl-0">
+
+        <img src="" alt="" width="auto" height="auto" >
+        <span></span>
+      </a>
+	  
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+	 <!-- Nav Menu -->
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href=" " class="active" ; style= "font-weight: bold">Home</a></li>
+          <li><a href=" " style="color : #18A0FB ; font-weight: bold">Post</a></li>
+          <li><a href=" " style="color : #18A0FB ; font-weight: bold">Publication</a></li>
+          <li><a href=" " style="color : #18A0FB ; font-weight: bold">Profile</a></li>
+          <li><a href=" " style="color : #18A0FB ; font-weight: bold">Logout</a></li>
+        </ul>
+
+      
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav><!-- End Nav Menu -->
+	<div class="sidebar">
+      <div class="sidebar-item search-form">
+                <form action="" class="mt-3">
+                  <input type="text">
+                  <button type="submit"><i class="bi bi-search"></i></button>
+                </form>
+              </div><!-- End sidebar search form-->
+
+    </div>
+  </header><!-- End Header Second -->
+  
+<hr>
+     
+
+</body>
+
+</html>
