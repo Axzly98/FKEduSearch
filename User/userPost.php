b4ad3740327a5b210292b0777e2268025d516e72
heavylappy <heavylappy@gmail.com>
2023-06-06 12:15:54 +0800

    ...

---
+++ User/userPost.php
@@ -0,0 +1,23 @@
+<?php
+$page = 'home';
+include 'header.php';
+?>
+
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            Discussion Board
+        </div>
+        <div class="card-body">
+            Test
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
