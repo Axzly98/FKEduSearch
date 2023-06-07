5451c6e6476d3b9c081784966b249cd51d1f6a2f
AniZ27 <105964251+AniZ27@users.noreply.github.com>
2023-06-07 13:55:38 +0800

    Add files via upload

--- Complaint/User/AddComplaintInterface.php
+++ Complaint/User/AddComplaintInterface.php
@@ -32,10 +32,10 @@ $row = mysqli_fetch_assoc($result);
 <!-- HEADER -->
 <div class="topnav">
   <a><img src="https://umplive.ump.edu.my/images/2020/07/26/logo-ump-transparent-blue__1122x561.png" style="width: 40px;"></a>
-  <a href="#home" style="margin-left: 400px;">Home</a>
-  <a href="#news">Your Post</a>
+  <a href="/FKEduSearch/User/userHome.php" style="margin-left: 400px;">Home</a>
+  <a href="/FKEduSearch/User/userYourPost.php">Your Post</a>
   <a class="active" href="/FKEduSearch/Complaint/User/ComplaintInterface.php">Complaint</a>
-  <a href="#about">Profile</a>
+  <a href="/FKEduSearch/User/userProfile.php">Profile</a>
   <a href="#about">Logout</a>
   <div class="search-container">
     <form action="/action_page.php">
