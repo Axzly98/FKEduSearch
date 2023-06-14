078ba45441f4d059e1e0fe433241f6774a5df13a
amin <55675998+amin.ramli@users.noreply.github.com>
2023-06-14 11:31:11 +0800

    ...

--- Admin/tghBaiki/register.php
+++ Admin/tghBaiki/register.php
@@ -5,46 +5,51 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fullName = $_POST['fname'];
     $role = $_POST['role'];
     $email = $_POST['email'];
-	$password = $_POST['password'];
+    $password = $_POST['password'];
 
     // Perform necessary validations and checks here
 
     $link = mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
     mysqli_select_db($link, "miniproject") or die(mysqli_error($link));
 
-    // Insert dalam tables masing2 based on the selected role
+    // Insert into the respective tables based on the selected role
     switch ($role) {
         case 'Admin':
+            // Insert into the admin table
             $adminQuery = "INSERT INTO admin (admin_userName, admin_password, admin_email) 
                            VALUES ('$username', '$password', '$email')";
             mysqli_query($link, $adminQuery);
             $adminId = mysqli_insert_id($link);
-			// insert jugak dalam login table...sebab nak synchronize kan username dengan password mase login nanti
-            $loginQuery = "INSERT INTO login ('admin_ID', login_userName, login_password,)
-                           VALUES ('$adminId', '$username', '$password')";
+
+            // Insert into the login table with the admin role_id
+            $loginQuery = "INSERT INTO login (role_id, admin_ID, login_userName, login_password) 
+                           VALUES ('1', '$adminId', '$username', '$password')";
             mysqli_query($link, $loginQuery);
             break;
 
         case 'Expert':
-           $expertQuery = "INSERT INTO expert (expert_userName, expert_password, expert_email, expert_fullName) 
+            // Insert into the expert table
+            $expertQuery = "INSERT INTO expert (expert_userName, expert_password, expert_email, expert_fullName) 
                             VALUES ('$username', '$password', '$email', '$fullName')";
-
             mysqli_query($link, $expertQuery);
             $expertId = mysqli_insert_id($link);
 
-            $loginQuery = "INSERT INTO login (expert_ID,, login_userName, login_password)
-                           VALUES ('$expertId', '$username', '$password')";
+            // Insert into the login table with the expert role_id
+            $loginQuery = "INSERT INTO login (role_id, expert_ID, login_userName, login_password) 
+                           VALUES ('2', '$expertId', '$username', '$password')";
             mysqli_query($link, $loginQuery);
             break;
 
         case 'User':
+            // Insert into the user table
             $userQuery = "INSERT INTO user (user_userName, user_password, user_email, user_fullName) 
-                          VALUES ('$username', '$password', '$email','$fullName')";
+                          VALUES ('$username', '$password', '$email', '$fullName')";
             mysqli_query($link, $userQuery);
             $userId = mysqli_insert_id($link);
 
-            $loginQuery = "INSERT INTO login (user_ID, login_userName, login_password)
-                           VALUES ('$userId', '$username', '$password')";
+            // Insert into the login table with the user role_id
+            $loginQuery = "INSERT INTO login (role_id, user_ID, login_userName, login_password) 
+                           VALUES ('3', '$userId', '$username', '$password')";
             mysqli_query($link, $loginQuery);
             break;
 
@@ -56,8 +61,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Close the database connection
     mysqli_close($link);
 
-    $alert_message = "Done Registration New Account !!!";	
-		echo "<script>alert('$alert_message');</script>";
-		 echo "<script type = 'text/javascript'> window.location='indexAdmin.php' </script>";
+    $alert_message = "Done Registration New Account !!!";
+    echo "<script>alert('$alert_message');</script>";
+    echo "<script type='text/javascript'>window.location='indexAdmin.php'</script>";
 }
 ?>
