0d61dd36e0bee7ca3a5b730927f76c7a279068ef
frshaneefa <108619796+frshaneefa@users.noreply.github.com>
2023-06-14 12:15:38 +0800

    ...

--- Admin/admin.php
+++ Admin/admin.php
@@ -1,112 +1,68 @@
 <?php
-	include 'headerAdmin.php';
+//_SERVER REQUEST METHOD...LEBIH KURANG MACAM ISSET....UNTUK CHECKED SUBMITTED FORM
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['uname'];
+    $fullName = $_POST['fname'];
+    $role = $_POST['role'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Perform necessary validations and checks here
+
+    $link = mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
+    mysqli_select_db($link, "miniproject") or die(mysqli_error($link));
+
+    // Insert into the respective tables based on the selected role
+    switch ($role) {
+        case 'Admin':
+            // Insert into the admin table
+            $adminQuery = "INSERT INTO admin (admin_userName, admin_password, admin_email) 
+                           VALUES ('$username', '$password', '$email')";
+            mysqli_query($link, $adminQuery);
+            $adminId = mysqli_insert_id($link);
+
+            // Insert into the login table with the admin role_id
+            $loginQuery = "INSERT INTO login (role_id, admin_ID, login_userName, login_password) 
+                           VALUES ('1', '$adminId', '$username', '$password')";
+            mysqli_query($link, $loginQuery);
+            break;
+
+        case 'Expert':
+            // Insert into the expert table
+            $expertQuery = "INSERT INTO expert (expert_userName, expert_password, expert_email, expert_fullName) 
+                            VALUES ('$username', '$password', '$email', '$fullName')";
+            mysqli_query($link, $expertQuery);
+            $expertId = mysqli_insert_id($link);
+
+            // Insert into the login table with the expert role_id
+            $loginQuery = "INSERT INTO login (role_id, expert_ID, login_userName, login_password) 
+                           VALUES ('2', '$expertId', '$username', '$password')";
+            mysqli_query($link, $loginQuery);
+            break;
+
+        case 'User':
+            // Insert into the user table
+            $userQuery = "INSERT INTO user (user_userName, user_password, user_email, user_fullName) 
+                          VALUES ('$username', '$password', '$email', '$fullName')";
+            mysqli_query($link, $userQuery);
+            $userId = mysqli_insert_id($link);
+
+            // Insert into the login table with the user role_id
+            $loginQuery = "INSERT INTO login (role_id, user_ID, login_userName, login_password) 
+                           VALUES ('3', '$userId', '$username', '$password')";
+            mysqli_query($link, $loginQuery);
+            break;
+
+        default:
+            // Handle the case when no role is selected
+            break;
+    }
+
+    // Close the database connection
+    mysqli_close($link);
+
+    $alert_message = "Done Registration New Account !!!";
+    echo "<script>alert('$alert_message');</script>";
+    echo "<script type='text/javascript'>window.location='indexAdmin.php'</script>";
+}
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>ADMIN</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body align="center">
-	<form action="admin.php" method="post">
-		<h2>Registered New User</h2>
-		
-		<?php if(isset($_GET['error'])) { ?>
-			<p class="error"><?php echo $_GET['error']; ?></p>
-		<?php } ?>
-		
-		<label>Username</label>
-		<input type="text" name="uname" placeholder="Username"><br><br>
-		<label>Full Name</label>
-		<input type="text" name="fname" placeholder="Fullname"><br><br>
-		
-		<select name="role">
-			<option value="">Role</option>
-			<option value="admin">Admin</option>
-			<option value="expert">Expert</option>
-			<option value="student">Student</option>
-		</select><br><br>
-		
-		<label>Email</label>
-		<input type="text" name="email" placeholder="Email"><br><br>
-		
-		<p><a href="#">Log in here</a></p>
-		<button type="submit" name="addRegistration">Add Registration</button><br><br>
-	</form>
-
-	<table border='1' align="center">
-		<tr>
-			<td>No.</td>
-			<td>Username</td>
-			<td>Fullname</td>
-			<td>Role</td>
-			<td>Email</td>
-			<td colspan='2'>Actions</td>
-		</tr>
-
-		<?php
-		// Connect to your database
-		$dbhost = "localhost";  // Replace with your database host
-		$dbuser = "username";   // Replace with your database user
-		$dbpass = "password";   // Replace with your database password
-		$dbname = "database";   // Replace with your database name
-		$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-		if (!$conn) {
-			die("Connection failed: " . mysqli_connect_error());
-		}
-
-		// Check if the addRegistration form is submitted
-		if (isset($_POST['addRegistration'])) {
-			// Get form data
-			$username = $_POST['uname'];
-			$fullname = $_POST['fname'];
-			$role = $_POST['role'];
-			$email = $_POST['email'];
-
-			// Validate form data (you can add your own validation checks here)
-
-			// Insert the new registration into the database
-			$sql = "INSERT INTO registrations (username, fullname, role, email) VALUES ('$username', '$fullname', '$role', '$email')";
-			if (mysqli_query($conn, $sql)) {
-				// Redirect back to admin.php with success message
-				header("Location: admin.php?success=Registration added successfully");
-				exit;
-			} else {
-				// Redirect back to admin.php with error message
-				header("Location: admin.php?error=Error adding registration: " . mysqli_error($conn));
-				exit;
-			}
-		}
-
-		// Fetch and display existing registrations from the database
-		$sql = "SELECT * FROM registrations";
-		$result = mysqli_query($conn, $sql);
-
-        if (mysqli_num_rows($result) > 0) {
-			// Loop through each row of data
-			while ($row = mysqli_fetch_assoc($result)) {
-				echo "<tr>";
-				echo "<td>" . $row['id'] . "</td>";
-				echo "<td>" . $row['username'] . "</td>";
-				echo "<td>" . $row['fullname'] . "</td>";
-				echo "<td>" . $row['role'] . "</td>";
-				echo "<td>" . $row['email'] . "</td>";
-				echo "<td><a href='view.php?id=" . $row['id'] . "'>View</a></td>";
-				echo "<td><a href='delete.php?id=" . $row['id'] . "'>Delete</a></td>";
-				echo "</tr>";
-			}
-		} else {
-			echo "<tr><td colspan='7'>No registrations found.</td></tr>";
-		}
-
-		mysqli_close($conn);
-		?>
-	</table>
-</body>
-</html>
-
-<?php
-	include 'footerAdmin.php';
-?>
-
