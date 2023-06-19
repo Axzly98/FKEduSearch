ecb2a89bab598f74fe4f127f6e0d796641771576
amin <55675998+amin.ramli@users.noreply.github.com>
2023-06-19 20:19:27 +0800

    ....

--- Expert/expertHome.php
+++ Expert/expertHome.php
@@ -7,8 +7,6 @@ include 'headerExpert.php';
 $link = mysqli_connect("localhost", "root") or die(mysqli_connect_error());
 mysqli_select_db($link, "miniproject") or die(mysqli_error());
 
-// untuk link kan dengan expert account yang kita login...gune session 
-//kena letak ni sebelom query yg dalam tag php......WAJIB !!!
 $expertID = $_SESSION["expertID"];
 
 $query = "SELECT COUNT(*) as total FROM publication where expert_ID = '$expertID' ";
@@ -22,9 +20,7 @@ $row = mysqli_fetch_assoc($result);
 $rating_sum = $row['total_rating'];
 $rating_count = $row['rating_count'];
 
-// Kira average ratings
 $average_rating = ($rating_count > 0) ? ($rating_sum / $rating_count) : 0;
-
 ?>
 
 <!DOCTYPE html>
@@ -32,186 +28,197 @@ $average_rating = ($rating_count > 0) ? ($rating_sum / $rating_count) : 0;
 
 <head>
   <style>
-   .publicationChart {
-  width: 50%;
-  height: 400px;
-  margin-bottom: 50px;
-  margin-left: auto;
-  margin-right: 0;
-  
-}
+    body {
+      font-family: Arial, sans-serif;
+    }
 
+    h1,
+    h2,
+    h3 {
+      text-align: center;
+    }
+
+    h1, h2 {
+      text-decoration: underline;
+    }
+
+    table {
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 10px;
+    }
+
+    th,
+    td {
+      padding: 10px;
+    }
+
+    .publicationChart {
+      width: 50%;
+      height: 400px;
+      margin-bottom: 50px;
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    .rating-form {
+      text-align: center;
+      margin-top: 20px;
+    }
+
+    .rating-form input[type="number"] {
+      width: 100px;
+      height: 25px;
+      padding: 5px;
+      font-size: 14px;
+      border-radius: 5px;
+    }
+
+    .rating-form input[type="submit"] {
+      background-color: #18A0FB;
+      color: #FFFFFF;
+      border: none;
+      border-radius: 5px;
+      width: 70px;
+      height: 25px;
+      font-size: 14px;
+    }
+
+    .rating-summary {
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 20px;
+      width: 300px;
+    }
+
+    .expert-details {
+      margin-top: 20px;
+      text-align: center;
+    }
+
+    .expert-details h3 {
+      font-size: 18px;
+      margin-bottom: 10px;
+    }
+
+    .expert-details table {
+      margin: 0 auto;
+      width: 300px;
+      border-collapse: collapse;
+    }
+
+    .expert-details th,
+    .expert-details td {
+      padding: 8px;
+      border: 1px solid #ddd;
+    }
   </style>
 </head>
 
 <body>
+  <h1>Summary</h1>
+  <h2>Display Academic Details</h2>
 
 
+  <?php
+  $expertID = $_SESSION['expertID'];
 
-<h1 style="margin-left: 20%;">Summary</h1>
+  $link = mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
+  mysqli_select_db($link, "miniproject") or die(mysqli_error($link));
 
-<table border="1" style="margin-left: 15%">
+  $queryDetails = "
+      SELECT a.academicStatus_type, e.expert_fullName
+      FROM academic_statususerexpert ase
+      INNER JOIN academic_status a ON FIND_IN_SET(a.academicStatus_ID, ase.academicStatus_ID)
+      INNER JOIN expert e ON ase.expert_ID = e.expert_ID
+      WHERE ase.expert_ID = '$expertID'
+  ";
 
-<tr>
-<th>Display Academic Details (Expert) Sini</th>
-<tr>
+  $resultDetails = mysqli_query($link, $queryDetails);
 
-<tr>
-<td align="center">"List Academic Details"</td>
+  if (mysqli_num_rows($resultDetails) > 0) {
+    $row = mysqli_fetch_assoc($resultDetails);
+    $academicStatuses = $row['academicStatus_type'];
+    $expertFullName = $row['expert_fullName'];
 
-</tr>
-
-</table>
-
-
-
-<br><br>
-				
-
-  <!-- Guna <div> element for the chart -->
-<div class="publicationChart" style="width: 50%; height: 400px; margin-bottom: 50px;" data-expertid="<?php echo $expertID; ?>"></div>
-
-		<h3 style="margin-left: 20%;">Rating (Calculation Part)</h3>
-		<h3 style="margin-left: 20%;">Untuk Gambaran</h3>
-		
-		<!-- form action = "" <--- maksud nye dia submit data dalam page yang same -->
-		<form action="" method="post">
-		<table border="0" style="margin-left: 15%">
-		    <tr>
-			<td><input type="number" name="rating_number" placeholder="Enter Rating"></td>
-			<td><input type="submit" style="background-color: #18A0FB; color: #FFFFFF; border-radius: 5px; width: 70px; height: 25px; font-size: 14px;" value="SUBMIT"></td>
-			</tr>
-
-		<tr>
-		<th>Current Total Rating: <?php echo $rating_sum ?> </th>
-		</tr>	
-		
-		<tr>
-		<th>Number of Users Rate: <?php echo $rating_count ?> </th>
-		</tr>
-	
-		<tr>
-		<th>Calculation </th>
-		</tr>
-		
-		<tr>
-		 <td><?php echo $rating_sum . " / "  . $rating_count . " = " . $average_rating ?> </td>
-		</tr>
-		
-		 
-		 <?php
-		 
-	  $link = mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
-      mysqli_select_db($link, "miniproject") or die(mysqli_error($link));
-		 
-    // gune isset untuk elak dari automatically insert ... isset = untuk check input dah fill in ke belom
-    if (isset($_POST['rating_number'])) {
-     
-	  $expertID = $_SESSION["expertID"];
-      $rating_number = $_POST["rating_number"];
-      $query = "INSERT INTO rating VALUES('', '$expertID', '', '', '$rating_number', '')";
-
-	
-	/* untuk reference...SQL Statement 
-	$query = "INSERT INTO publication (publicationTitle, publicationDate, publisherName, publicationType, publicationFile)
-              VALUES ('$title', '$publicationCreatedDate', '$name', '$type', '')" */
-    
-  
-	$result = mysqli_query($link, $query);
-	
-	if($result) {
-	$alert_message = "Rating Has Been Entered!";
-	echo "<script>alert('$alert_message');</script>";
-	echo "<script type='text/javascript'>window.location='expertHome.php'</script>";
-	} else {
-	$alert_message = "Rating Not Entered!";
-	echo "<script>alert('$alert_message');</script>";
-	echo "<script type='text/javascript'>window.location='expertHome.php'</script>";
-	}
+    $academicStatusArray = explode(",", $academicStatuses);
+    echo "<div class='expert-details'>";
+    echo "<h3>Expert's Full Name: $expertFullName</h3>";
+    echo "<table>";
+    echo "<tr><th>Expert's Academic Details:</th><td>";
+    foreach ($academicStatusArray as $academicStatus) {
+      echo "$academicStatus<br>";
+    }
+    echo "</td></tr>";
+    echo "</table>";
+    echo "</div>";
+  } else {
+    echo "No records found.";
   }
-// Close the database connection
-mysqli_close($link);
 
-?>
+  mysqli_close($link);
+  ?>
 
-	
-		
-		
-		
-		</table>
-		</form>
-		
-		
-		
-		
-		
-	
-				
-  <!-- Gune Highcharts library...boleh cari dari google -->
+  <div class="publicationChart" data-expertid="<?php echo $expertID; ?>"></div>
+
   <script src="https://code.highcharts.com/highcharts.js"></script>
 
+  <script>
+    var container = document.querySelector('.publicationChart[data-expertid="<?php echo $expertID; ?>"]');
 
-
-  <!-- Gune JavaScript code to create the bar chart -->
-<script>
-  // guna name variable container ..untuk dapatkan .publicationChart dari atas.....
-  var container = document.querySelector('.publicationChart[data-expertid="<?php echo $expertID; ?>"]');
-
-
-  // Buat bar chart gune Highcharts
-  Highcharts.chart(container, {
-    chart: {
-      type: 'column'
-    },
-    title: {
-      text: 'Total Publications, Total Ratings and Average Ratings'
-    },
-    xAxis: {
-      categories: ['']
-    },
-    yAxis: {
+    Highcharts.chart(container, {
+      chart: {
+        type: 'column'
+      },
       title: {
-        text: ''
+        text: 'Total Publications, Total Ratings, and Average Ratings'
+      },
+      xAxis: {
+        categories: ['']
+      },
+      yAxis: {
+        title: {
+          text: ''
+        }
+      },
+      series: [{
+        name: 'Publications',
+        data: [{
+          y: <?php echo $totalPublications; ?>,
+          dataLabels: {
+            enabled: true,
+            format: 'Total Publications: <?php echo $totalPublications; ?>'
+          }
+        }],
+        color: 'rgba(75, 192, 192, 0.8)'
+      }, {
+        name: 'Total Ratings',
+        data: [{
+          y: <?php echo $rating_sum; ?>,
+          dataLabels: {
+            enabled: true,
+            format: 'Total Ratings: <?php echo $rating_sum; ?>'
+          }
+        }],
+        color: 'rgba(192, 75, 75, 0.8)'
+      }, {
+        name: 'Average Ratings',
+        data: [{
+          y: <?php echo $average_rating; ?>,
+          dataLabels: {
+            enabled: true,
+            format: 'Average Ratings: {point.y:.2f}'
+          }
+        }],
+        color: 'turquoise'
+      }],
+      plotOptions: {
+        column: {
+          pointWidth: 30
+        }
       }
-    },
-   series: [{
-      name: 'Publications',
-      data: [{
-        y: <?php echo $totalPublications; ?>,
-        dataLabels: {
-          enabled: true,
-          format: 'Total Publications: <?php echo $totalPublications; ?>'
-        }
-      }],
-      color: 'rgba(75, 192, 192, 0.8)'
-    }, {
-      name: 'Total Ratings',
-      data: [{
-        y: <?php echo $rating_sum; ?>,
-        dataLabels: {
-          enabled: true,
-          format: 'Total Ratings: <?php echo $rating_sum; ?>'
-        }
-      }],
-      color: 'rgba(192, 75, 75, 0.8)'
-    }, {
-      name: 'Average Ratings',
-      data: [{
-        y: <?php echo $average_rating; ?>,
-        dataLabels: {
-          enabled: true,
-            format: 'Average Ratings: {point.y:.2f}' // Format to three decimal points
-        }
-      }],
-      color: 'turquoise'
-    }],
-    plotOptions: {
-      column: {
-        pointWidth: 30 // Adjust width bar chart 
-      }
-    }
-  });
-</script>
+    });
+  </script>
 
 </body>
 
