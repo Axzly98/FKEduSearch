08eb99243b4cb68aa9f543059a4f9628ec46f922
frshaneefa <108619796+frshaneefa@users.noreply.github.com>
2023-06-20 01:47:27 +0800

    ...

--- Admin/ReactivateAcc.php
+++ Admin/ReactivateAcc.php
@@ -1,32 +1,35 @@
 <?php
-	include'headerAdmin.php';
+	include 'headerAdmin.php';
+
+	// Establish a database connection (replace the placeholder values with your actual credentials)
+	$link = mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
+    mysqli_select_db($link, "miniproject") or die(mysqli_error($link));
+
+	// Fetch data from the admin, user, and expert tables
+	$query = "SELECT * FROM admin UNION SELECT * FROM user UNION SELECT * FROM expert";
+	$result = mysqli_query($connection, $query);
+
+	// Check if there are any rows returned
+	if (mysqli_num_rows($result) > 0) {
+		$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	} else {
+		$users = [];
+	}
+
+	// Close the database connection
+	mysqli_close($connection);
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>ADMIN</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
-	<div>
-		<h5>Universiti Malaysia Pahang</h5>
-	</div>
-	<div>
-		<table>
-			<tr>
-				<td><a href="#">Reactivate Acc</a></td>
-				<td><a href="#">Complaint</td>
-			</tr>
-			<tr>
-				<td><a href="#">Manage Acc</td>
-				<td><a href="#">Logout</td>
-			</tr>
-		</table>
-	</div>
+<body align="center">
+	<h2>Registered New Users</h2>
 	
-	<h2>Registerd New User</h2>
-	
-	<table border='1'>
+	<table border='1' align="center">
 		<tr>
 			<td>No.</td>
 			<td>Username</td>
@@ -35,27 +38,22 @@
 			<td>Acc Status</td>
 			<td colspan='2'>Reactivate Approval</td>
 		</tr>
-		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-		</tr>
-		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-		</tr>
+		
+		<?php foreach ($users as $index => $user): ?>
+			<tr>
+				<td><?php echo $index + 1; ?></td>
+				<td><?php echo $user['username']; ?></td>
+				<td><?php echo $user['id']; ?></td>
+				<td><?php echo $user['reason']; ?></td>
+				<td><?php echo $user['acc_status']; ?></td>
+				<td colspan='2'>Reactivate Approval</td>
+			</tr>
+		<?php endforeach; ?>
+		
 	</table>
 </body>
 </html>
+
 <?php
-	include'footerAdmin.php';
+	include 'footerAdmin.php';
 ?>
